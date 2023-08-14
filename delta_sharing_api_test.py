3c7a28eb75941430ea73aee0d7efd37b99a5f077
Jianyu Zhou <jianyu.zhou@databricks.com>
2023-08-14 11:29:33 -0700

    .

--- delta_sharing_api_test.py
+++ delta_sharing_api_test.py
@@ -1,11 +1,11 @@
 import requests
 
 # Define the Databricks domain and endpoint
-DATABRICKS_DOMAIN = "https://<YOUR-DATABRICKS-WORKSPACE-URL>"
-ENDPOINT = "/api/2.0/workspace/shares/list"
+DATABRICKS_DOMAIN = "https://dbc-64aa1604-7419.dev.databricks.com"
+ENDPOINT = "/api/2.1/unity-catalog/shares"
 
 # Your personal access token or API token
-TOKEN = "Bearer <YOUR-TOKEN>"
+TOKEN = "Bearer dapi1b05f73b8ee741eccd1de61430a8fd47"
 
 # Set up the headers for the request
 headers = {
